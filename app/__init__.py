92482ac104bdf4d5321266e5cdc943209f53ea6a
Your Name <your_email@example.com>
2025-07-20 15:56:10 -0700

    second push

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+##== Initializing the Flask application ==
 app = Flask(__name__)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    #== Fake posts in view function ==
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!',
+            'age': 25
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!',
+            'age': 30
+        }
+        
+    ]
+    return render_template('index.html', title='Home Page', user=user, posts = posts)
